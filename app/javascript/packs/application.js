69a7c078741970891f8dddedd118c560c41a8dab
sasha370 <kng.sasha@mail.ru>
2020-08-09 10:24:36 +0300

    update gem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
@@ -15,12 +14,7 @@ require("semantic-ui-sass")
 // require("semantic-ui")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-// $(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
-// $('.message .close').on('click', () => $(this).closest('.message').transition('fade'));
+
 
 scroll_bottom = function() {
     if ($('#messages').length > 0) {
